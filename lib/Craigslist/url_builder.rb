a7cf96331b58d3e87a967f5406e9de130793eeef
ferrisbueller66 <tmtarpinian@gmail.com>
2024-07-08 23:59:04 -0400

    move url to own class

---
+++ lib/Craigslist/url_builder.rb
@@ -0,0 +1,15 @@
+module Craigslist
+  class UrlBuilder
+    attr_reader :url, :query_value
+    def initialize(city:, site_section:, query_value: nil)
+      @url = build(city, site_section, query_value)
+    end
+
+    private
+
+    def build(city, site_section, query_value)
+      url = "https://#{CITIES[city.upcase.to_sym]}.craigslist.org/search/#{SITE_SECTIONS[site_section.upcase.to_sym]}"
+      query_value.nil? ? url : url.concat("?query=#{query_value}")
+    end
+  end
+end
