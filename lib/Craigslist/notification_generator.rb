d7079e5276b0a6d10befd198995dce13c24765a5
ferrisbueller66 <tmtarpinian@gmail.com>
2024-07-11 23:08:06 -0400

    implement redis and logging

--- lib/Craigslist/notification_generator.rb
+++ lib/Craigslist/notification_generator.rb
@@ -1,6 +1,8 @@
+require 'aws-sdk-lambda'
+require 'logger'
 require 'net/http'
 require 'nokogiri'
-require 'aws-sdk-lambda'
+require 'redis'
 require_relative 'index'
 
 module Craigslist
@@ -14,6 +16,7 @@ module Craigslist
       @search_keywords = opts[:search_keywords]
       @city = opts[:city]
       validate_opts(opts)
+      Logger.new($stdout).info("\nValidated options successfully...\n")
     end
     
     def run
@@ -21,8 +24,11 @@ module Craigslist
         city: city,
         site_section: site_section,
         query_value: query_value).url
+        Logger.new($stdout).info("\nCraigslist URL: #{craigslist_url}\n")
       html = scrape_craigslist(craigslist_url)
+      Logger.new($stdout).info("\nScraped Craigslist URL successfully...\n")
       craigslist_postings = Craigslist::PostingsBuilder.new(self, html).parse_postings_in_html
+      Logger.new($stdout).info("\nParsed Craigslist postings successfully...\n")
       # craigslist_postings => {
       #     title: title,
       #     date: Time.now.strftime('%m-%d-%Y'),
@@ -30,8 +36,36 @@ module Craigslist
       #     location: format_location(result),
       #     price: format_price(result),
       #     pid: format_pid(result),
+      #     keyword: keyword
       #   }
-      send("trigger_#{notification_type}_notifications", craigslist_postings) unless craigslist_postings.empty?
+    
+     begin
+     # Set up Redis connection
+     redis = Redis.new(
+       host: ENV['REDIS_HOST'],
+       port: ENV['REDIS_PORT'].to_i
+     )
+     rescue => e
+       logger.error("Error connecting to Redis: #{e.message}")
+     end
+
+      #TODO : refactor into redis check and notification trigger methods
+      craigslist_postings.each do |craigslist_posting|
+        pid = craigslist_posting[:pid]
+
+
+
+        cached_pid = redis.get(pid)
+        Logger.new($stdout).info("\nCached PID: #{cached_pid}")
+        if cached_pid
+          puts "PID #{pid} found in cache, skipping..."
+          next
+        else
+          redis.set(pid, DateTime.now.to_s, ex: 35 * 24 * 60 * 60) # Expire after 35 days
+          send("trigger_#{notification_type}_notifications", craigslist_posting)
+        end
+      end
+
     end
 
     private
@@ -44,12 +78,16 @@ module Craigslist
     end
 
     def scrape_craigslist(craigslist_url)
+      begin
       uri = URI(craigslist_url)
       Net::HTTP.get(uri)
+      rescue => e
+        Logger.new($stdout).info("\nHTTP request failed #{e}")
+      end
     end
 
-    def trigger_email_notifications(craigslist_postings)
-      craigslist_postings.each { |craigslist_posting| Craigslist::InvokeEmailNotificationLambda.new(craigslist_posting) }
+    def trigger_email_notifications(craigslist_posting)
+      Craigslist::InvokeEmailNotificationLambda.new(craigslist_posting)
     end
   end
 end
