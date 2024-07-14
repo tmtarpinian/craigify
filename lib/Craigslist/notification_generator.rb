e2c9426ee098fe25bd28cac2be9fc4e16d4ea15b
ferrisbueller66 <tmtarpinian@gmail.com>
2024-07-13 22:53:53 -0400

    update specs

--- lib/Craigslist/notification_generator.rb
+++ lib/Craigslist/notification_generator.rb
@@ -23,55 +23,21 @@ module Craigslist
       craigslist_url = Craigslist::UrlBuilder.new(
         city: city,
         site_section: site_section,
-        query_value: query_value).url
-        Logger.new($stdout).info("\nCraigslist URL: #{craigslist_url}\n")
+        query_value: query_value
+      ).url
+
+      Logger.new($stdout).info("\nCraigslist URL: #{craigslist_url}\n")
       html = scrape_craigslist(craigslist_url)
       Logger.new($stdout).info("\nScraped Craigslist URL successfully...\n")
       craigslist_postings = Craigslist::PostingsBuilder.new(self, html).parse_postings_in_html
       Logger.new($stdout).info("\nParsed Craigslist postings successfully...\n")
-      # craigslist_postings => {
-      #     title: title,
-      #     date: Time.now.strftime('%m-%d-%Y'),
-      #     url: format_href(result),
-      #     location: format_location(result),
-      #     price: format_price(result),
-      #     pid: format_pid(result),
-      #     keyword: keyword
-      #   }
-    
-     begin
-     # Set up Redis connection
-     redis = Redis.new(
-       host: ENV['REDIS_HOST'],
-       port: ENV['REDIS_PORT'].to_i
-     )
-     rescue => e
-       logger.error("Error connecting to Redis: #{e.message}")
-     end
-
-      #TODO : refactor into redis check and notification trigger methods
-      craigslist_postings.each do |craigslist_posting|
-        pid = craigslist_posting[:pid]
-
-
-
-        cached_pid = redis.get(pid)
-        Logger.new($stdout).info("\nCached PID: #{cached_pid}")
-        if cached_pid
-          puts "PID #{pid} found in cache, skipping..."
-          next
-        else
-          redis.set(pid, DateTime.now.to_s, ex: 35 * 24 * 60 * 60) # Expire after 35 days
-          send("trigger_#{notification_type}_notifications", craigslist_posting)
-        end
-      end
-
+      trigger_notification(craigslist_postings)
     end
 
     private
 
     def validate_opts(opts)
-      raise ArgumentError, 'Missing required arguments' unless opts[:city] && opts[:site_section] && opts[:notification_type]
+      raise ArgumentError, 'Missing city, site_secion, or notification type' unless opts[:city] && opts[:site_section] && opts[:notification_type]
       raise ArgumentError, 'Invalid site section' unless SITE_SECTIONS.keys.include?(opts[:site_section].upcase.to_sym)
       raise ArgumentError, 'Invalid city' unless CITIES.keys.include?(opts[:city].upcase.to_sym)
       raise ArgumentError, 'Invalid notification type' unless NOTIFICATION_TYPES.include?(notification_type)
@@ -85,6 +51,43 @@ module Craigslist
         Logger.new($stdout).info("\nHTTP request failed #{e}")
       end
     end
+
+    def trigger_notification(craigslist_postings)
+      redis = initialize_redis 
+      craigslist_postings.each do |craigslist_posting|
+        # craigslist_posting => {
+        #     title: title,
+        #     date: Time.now.strftime('%m-%d-%Y'),
+        #     url: format_href(result),
+        #     location: format_location(result),
+        #     price: format_price(result),
+        #     pid: format_pid(result),
+        #     keyword: keyword
+        #   }
+
+        cached_pid = redis.get(craigslist_posting[:pid])
+        if cached_pid
+          puts "PID #{craigslist_posting[:pid]} found in cache, skipping post..."
+          next
+        else
+          redis.set(craigslist_posting[:pid], DateTime.now.to_s, ex: 35 * 24 * 60 * 60) # Expire after 35 days
+          send("trigger_#{notification_type}_notifications", craigslist_posting)
+        end
+      end
+    end
+
+    def initialize_redis
+      begin
+        # Set up Redis connection
+        Redis.new(
+          host: ENV['REDIS_HOST'],
+          port: ENV['REDIS_PORT'].to_i
+        )
+      rescue => e
+        logger.error("Error connecting to Redis: #{e.message}")
+      end
+    end
+
 
     def trigger_email_notifications(craigslist_posting)
       Craigslist::InvokeEmailNotificationLambda.new(craigslist_posting)
--- spec/Craigslist/postings_builder_spec.rb
+++ spec/Craigslist/postings_builder_spec.rb
@@ -22,37 +22,118 @@ RSpec.describe Craigslist::PostingsBuilder do
 
   describe '#parse_postings_in_html' do
     let(:site_section) {'LABOR_GIGS'}
-    let(:notification_generator) { Craigslist::NotificationGenerator.new({ city: 'DETROIT', site_section: site_section, notification_type: 'email' }) }
     let(:html) { File.read(File.join(File.dirname(__FILE__), 'labor_gigs_mock_response.html')) }
+    let(:notification_generator) { Craigslist::NotificationGenerator.new({ city: 'DETROIT', site_section: site_section, notification_type: 'email', search_keywords: ['handy'] }) }
+    let(:notification_generator_2) { Craigslist::NotificationGenerator.new({ city: 'DETROIT', site_section: site_section, notification_type: 'email', search_keywords: ['paint'] }) }
 
-    xit 'calls parse_craigslist_page' do
-      
+    it 'returns an array of postings' do
       postings_builder = described_class.new(notification_generator, html)
-      expect(postings_builder).to receive(:parse_craigslist_page).with(html)
-      # postings_builder.parse_postings_in_html
+      expect(postings_builder.parse_postings_in_html).to eq(
+        [
+          {
+            :date=>"07-14-2024",
+            :keyword=>"handy",
+            :location=>"Tireman",
+            :pid=>"7762543982",
+            :price=>"$0",
+            :title=>"Looking to hire handyman as soon as possible",
+            :url=>"https://detroit.craigslist.org/wyn/lbg/d/detroit-looking-to-hire-handyman-as/7762543982.html"
+          },
+          {
+            :date=>"07-14-2024",
+            :keyword=>"handy",
+            :location=>"Eastpointe",
+            :pid=>"7760206604",
+            :price=>"$0",
+            :title=>"Looking for handyman / contractors",
+            :url=>"https://detroit.craigslist.org/mcb/lbg/d/eastpointe-looking-for-handyman/7760206604.html"
+          },
+          {
+            :date=>"07-14-2024",
+            :keyword=>"handy",
+            :location=>"Eastside",
+            :pid=>"7759586911",
+            :price=>"$0",
+            :title=>"Handyman Wanted",
+            :url=>"https://detroit.craigslist.org/wyn/lbg/d/fraser-handyman-wanted/7759586911.html"
+          },
+          {
+            :date=>"07-14-2024",
+            :keyword=>"handy",
+            :location=>"macomb county",
+            :pid=>"7756245124",
+            :price=>"$0",
+            :title=>"skill repair/handyman for rental home improvement",
+            :url=> "https://detroit.craigslist.org/mcb/lbg/d/saint-clair-shores-skill-repair/7756245124.html"
+          }
+        ]
+      )
+      postings_builder = described_class.new(notification_generator_2, html)
+      expect(postings_builder.parse_postings_in_html).to eq(
+      [
+        {
+          :date=>"07-14-2024",
+          :keyword=>"paint",
+          :location=>"Detroit",
+          :pid=>"7761740004",
+          :title=>"Painter",
+          :price=>"$0",
+          :url=> "https://detroit.craigslist.org/wyn/lbg/d/detroit-painter/7761740004.html"
+        },
+        {
+          :date=>"07-14-2024",
+          :keyword=>"paint",
+          :location=>"Royal Oak",
+          :pid=>"7758606428",
+          :title=>"I need a painter and lawn care company",
+          :price=>"$0",
+          :url=> "https://detroit.craigslist.org/okl/lbg/d/royal-oak-need-painter-and-lawn-care/7758606428.html"
+        },
+        {
+          :date=>"07-14-2024",
+          :keyword=>"paint",
+          :location=>"Macomb County",
+          :pid=>"7758435698",
+          :title=>"FULL TIME PAINTER NEEDED",
+          :price=>"$0",
+          :url=> "https://detroit.craigslist.org/mcb/lbg/d/harrison-township-full-time-painter/7758435698.html"
+        },
+        {
+          :date=>"07-14-2024",
+          :keyword=>"paint",
+          :location=>"Detroit",
+          :pid=>"7758361566",
+          :title=>"Law services, gutters, and paint",
+          :price=>"$0",
+          :url=> "https://detroit.craigslist.org/wyn/lbg/d/detroit-law-services-gutters-and-paint/7758361566.html"
+        },
+        {
+          :date=>"07-14-2024",
+          :keyword=>"paint",
+          :location=>"Oakland County",
+          :pid=>"7758098814",
+          :price=>"$0",
+          :title=>"Hiring Skilled Painters",
+          :price=>"$0",
+          :url=>"https://detroit.craigslist.org/okl/lbg/d/royal-oak-hiring-skilled-painters/7758098814.html"},
+        {
+          :date=>"07-14-2024",
+          :keyword=>"paint",
+          :location=>"Pontiac",
+          :pid=>"7757978521",
+          :price=>"$0",
+          :title=>"Painter/ Trimmer",
+          :price=>"$0",
+          :url=>"https://detroit.craigslist.org/okl/lbg/d/pontiac-painter-trimmer/7757978521.html"
+        }
+      ]
+      )
     end
 
-    xit 'calls find_listings_with_search_term' do
-      
+    it 'returns an array of hashes with the correct keys' do
       postings_builder = described_class.new(notification_generator, html)
-      allow(postings_builder).to receive(:parse_craigslist_page).and_return([])
-      expect(postings_builder).to receive(:find_listings_with_search_term).with([])
-      # postings_builder.parse_postings_in_html
-    end
-
-    xit 'returns an array of postings' do
-      
-      postings_builder = described_class.new(notification_generator, html)
-
-      expect(postings_builder.parse_postings_in_html).to eq([{
-        title: 'Title',
-        date: Time.now.strftime('%m-%d-%Y'),
-        url: 'https://detroit.craigslist.org/lbg/d/handyman/123456.html',
-        location: 'Detroit',
-        price: '$10',
-        pid: '123456',
-        keyword: 'handyman'
-      }])
+      postings = postings_builder.parse_postings_in_html
+      expect(postings.first.keys).to eq([:title, :date, :url, :location, :price, :pid, :keyword])
     end
   end
 end
